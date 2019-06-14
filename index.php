cbdea55bd137f24963b858bb11d78a601aa108b6
Giusy Iaria <s265444@studenti.polito.it>
2019-06-14 12:05:28 +0200

    Added counter and index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
+<?php
+  include 'lib/utils.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
+<script src='js/utils.js'></script>
 <title>Check In</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="./css/home.css">
@@ -11,14 +15,18 @@
 </div>
 <div class="sidenav">
   <navcontent>
-  <a class="active" href="home.php">Home</a>
+  <a class="active" href="index.php">Home</a>
   <a href="login.php">Log in</a>
   <a href="signup.php">Sign up</a>
   </navcontent>
 </div>
 
 <div class="main">
-  
+    <div class="map"> 
+        <?php
+            loadMapForVisitor();
+        ?>
+    </div>
 </div>
    
 </body>
